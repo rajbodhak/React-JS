436592ba2c6cee3b8a9f77111734b270a4f124fb
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-19 14:53:22 +0530

    Manipulate Vite

---
+++ 01basicviteReact/src/Practice.jsx
@@ -0,0 +1,10 @@
+function Practice() {
+    return (
+        <>
+        <h1>Hey this is Import from Practice.jsx</h1>
+        <h2>Also this from Practice.jsx</h2>
+        </>
+    )
+}
+
+export default Practice;
--- 01basicviteReact/src/main.jsx
+++ 01basicviteReact/src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Practice from './Practice.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Practice />
+  </React.StrictMode>
+);
+
