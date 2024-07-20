5863045c8e962e3fc868756bb52b373dbb152fea
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-20 12:14:07 +0530

    Learn Basic JSX

--- 01basicviteReact/src/main.jsx
+++ 01basicviteReact/src/main.jsx
@@ -3,10 +3,33 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import Practice from './Practice.jsx';
 
+function MyApp() {
+  return (
+    <h1> Hello this is from myApp function</h1>
+  )
+};
+
+const anotherELement = (
+  <a href='https://google.com'>Click me to visit Google</a>
+);
+
+const user = 'John Cina';
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click to google --- ',
+  user
+)
+ 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-    <Practice />
-  </React.StrictMode>
+  // MyApp()
+  // <>
+  //   <MyApp/>
+  //   <App />
+  //   <Practice />
+  // </>
+  // anotherELement  
+  reactElement
 );
 
