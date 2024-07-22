fe5338a6294d4d23a6d54eed3a431a40866bdffd
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-22 19:03:14 +0530

    Start creating BG-Color changer

---
+++ 04-BG-ColorChange/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState("black")
+
+  function colorChange(color) {
+  
+  }
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
