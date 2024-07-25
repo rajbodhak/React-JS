8dc7b3d58eb6e182883f06e57eca5035182ac950
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-25 14:51:54 +0530

    Update useCurrencyInfo

--- 06-Currency-Converter/src/hooks/useCurrencyInfo.js
+++ 06-Currency-Converter/src/hooks/useCurrencyInfo.js
@@ -1,1 +1,15 @@
-importScripts
+import { useEffect, useState } from "react";
+
+const useCurrencyInfo = function(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(`https://v6.exchangerate-api.com/v6/15e3364f6763159013f02b14/latest/${currency}`)
+        .then((res) => res.json)
+        .then((res) => setData(res[conversion_rates]))
+    },[currency])
+
+    return data;
+}
+
+export  default useCurrencyInfo;
