3968d5bc7b8f7061604b2729d3616358a7d3b14c
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-25 19:28:15 +0530

    Update useCurrencyInfo.js

--- 06-Currency-Converter/src/hooks/useCurrencyInfo.js
+++ 06-Currency-Converter/src/hooks/useCurrencyInfo.js
@@ -5,10 +5,10 @@ const useCurrencyInfo = function(currency) {
 
     useEffect(() => {
         fetch(`https://v6.exchangerate-api.com/v6/15e3364f6763159013f02b14/latest/${currency}`)
-        .then((res) => res.json)
-        .then((res) => setData(res[conversion_rates]))
+        .then((res) => res.json())
+        .then((res) => setData(res["conversion_rates"]))
     },[currency])
-
+    console.log(data);
     return data;
 }
 
