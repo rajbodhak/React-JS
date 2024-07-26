0b2fc5cc19b87e1063d9d98658504899d4d13663
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-26 17:18:13 +0530

    Started Router Project

---
+++ 07-React-Router/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home/Home";
+import { Outlet } from "react-router-dom";
+
+
+function Layout() {
+    return(
+        <>
+           <Header/>
+            <Outlet/>
+           <Footer/>
+        </>
+    )
+}
+
+export default Layout;
---
+++ 07-React-Router/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Layout from './Layout.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import ContactUs from './Components/ContactUs/ContactUs.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "About",
+        element: <About/>
+      },
+      {
+        path: "ContactUs",
+        element: <ContactUs />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
