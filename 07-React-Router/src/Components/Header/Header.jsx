60a8539ea491bd623220c1285f1e5b609d16d609
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-26 17:24:31 +0530

    Add Contact Us File

--- 07-React-Router/src/Components/Header/Header.jsx
+++ 07-React-Router/src/Components/Header/Header.jsx
@@ -54,6 +54,17 @@ function Header() {
                                 </NavLink>
                             </li>
 
+                            <li>
+                                <NavLink
+                                    to='Contactus'
+                                    className={({isActive}) =>
+                                        `${isActive ? 'text-orange-700' : 'text-gray-700'} block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                    }
+                                >
+                                    Contact Us
+                                </NavLink>
+                            </li>
+
                         </ul>
                     </div>
                 </div>
