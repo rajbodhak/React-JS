15816e398420d43bd4b3410c3bb6ebc9d084cb48
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-26 11:38:51 +0530

    Final UI update

--- 06-Currency-Converter/src/App.jsx
+++ 06-Currency-Converter/src/App.jsx
@@ -25,27 +25,42 @@ function App() {
 
     return (
         <div
-            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            className="w-full h-screen flex justify-center items-center bg-cover bg-no-repeat"
             style={{
-                backgroundImage: `url('https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+                backgroundImage: `url('https://img.freepik.com/free-photo/futuristic-moon-background_23-2150930692.jpg?size=626&ext=jpg&ga=GA1.1.950715311.1711867512&semt=ais_user')`,
             }}
         >
-            <div className="w-full">
-                <div className="w-full max-w-lg mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+            <div className='flex flex-row space-x-4'>
+                <div className='w-96 h-auto p-6 border border-gray-60 backdrop-blur-sm bg-white/30 rounded-lg shadow-lg'>
+                    <h1 className='text-2xl font-bold text-blue-950 mb-4 text-center'>Currency Converter</h1>
+                    <div className='flex justify-center'>
+                    <img 
+                        src='https://cdn.shopify.com/app-store/listing_images/b33f6d876accac12b91af70e42dabbdb/icon/CK-cufzoue8CEAE=.png' 
+                        alt='Currency Converter' 
+                        className='w-52 h-36 object-cover mb-4 rounded border shadow-lgr'
+                    />
+                    </div>
+                    <p className='text-black'>
+                        Easily convert currencies from one form to another with real-time exchange rates. 
+                        Our currency converter provides accurate and up-to-date conversion values for over 150 currencies worldwide.
+                    </p>
+                </div>
+
+                <div className="w-96 h-auto p-6 border border-gray-60 rounded-lg backdrop-blur-sm bg-white/30 shadow-lg">
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
                             convert();
                         }}
                     >
-                        <div className="w-full mb-1">
+                        <div className="w-full mb-1 pt-10">
                             <InputBox
                                 label="From"
                                 amount={amount}
                                 currencyOptions={options}
                                 onCurrencyChange={(currency) => setFrom(currency)}
                                 selectCurrency={from}
-                                onAmountChange = {(amount) => setAmount(amount)}
+                                onAmountChange={(amount) => setAmount(amount)}
                             />
                         </div>
                         <div className="relative w-full h-0.5">
