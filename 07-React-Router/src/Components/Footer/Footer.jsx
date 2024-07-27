c0ba5e4912696b0208f336160fc47a9931e3e98a
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-27 13:25:51 +0530

    Update main and Footer file

--- 07-React-Router/src/Components/Footer/Footer.jsx
+++ 07-React-Router/src/Components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ function Footer() {
                             <ul className="text-gray-500 font-medium">
                                 <li className="mb-4">
                                     <a
-                                        href="https://github.com/hiteshchoudhary"
+                                        href="https://github.com/rajbodhak"
                                         className="hover:underline"
                                         target="_blank"
                                         rel="noreferrer"
--- 07-React-Router/src/main.jsx
+++ 07-React-Router/src/main.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
 import Layout from './Layout.jsx'
 import Home from './Components/Home/Home.jsx'
 import About from './Components/About/About.jsx'
 import ContactUs from './Components/ContactUs/ContactUs.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    children: [
-      {
-        path: "",
-        element: <Home/>
-      },
-      {
-        path: "About",
-        element: <About/>
-      },
-      {
-        path: "ContactUs",
-        element: <ContactUs />
-      }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home/>
+//       },
+//       {
+//         path: "About",
+//         element: <About/>
+//       },
+//       {
+//         path: "ContactUs",
+//         element: <ContactUs />
+//       }
+//     ]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout/>}>
+      <Route path='' element = {<Home/>}/>
+      <Route path='About' element = {<About/>}/>
+      <Route path='ContactUs' element = {<ContactUs/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
