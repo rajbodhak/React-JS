fc1d32114d03fc28b86cd97c8d301346f9f9bae4
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-28 01:34:36 +0530

    Complete Theme Project

--- 09-Theme-Changer/src/App.jsx
+++ 09-Theme-Changer/src/App.jsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { ThemeProvider } from './Context/theme'
+import ThemeBtn from './Components/ThemeBtn'
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
 
+  const darkTheme = () => {
+    setThemeMode('dark')
+  }
+
+  const lightTheme = () => {
+    setThemeMode('light')
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light', 'dark')
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
   return (
-   <>
-   <h1>Theme Changer</h1>
-   </>
+   <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {<ThemeBtn/>}
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {<Card/>}
+        </div>
+      </div>
+    </div>
+   </ThemeProvider>
   )
 }
 
