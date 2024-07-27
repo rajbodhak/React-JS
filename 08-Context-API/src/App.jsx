efeb3a5c31a411c704e7bd55c76cdf604a46cc2f
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-27 19:32:04 +0530

    Add Login Page

--- 08-Context-API/src/App.jsx
+++ 08-Context-API/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>Context API</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
