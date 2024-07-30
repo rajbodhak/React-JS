37a60f97811f612c0c86f1f9754ddd435c6d8e39
Raj Bodhak <rajlaptop2004@gmail.com>
2024-07-30 16:16:45 +0530

    Create Basic React app using vite

---
+++ 11-Redux-Todo/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-white font-bold text-4xl text-center pt-80'>Learning React Redux</h1>
+    </>
+  )
+}
+
+export default App
