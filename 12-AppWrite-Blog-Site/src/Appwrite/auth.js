13fed1c88f61c6eea1b34c66a971aabbd68c7164
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-06 13:53:40 +0530

    ADD auth file

---
+++ 12-AppWrite-Blog-Site/src/Appwrite/auth.js
@@ -0,0 +1,60 @@
+import config from '../config/config'
+import {Client, Account, ID} from 'appwrite'
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(config.appWriteUrl)
+            .setProject(config.appWriteProjectID)
+
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            
+            if (userAccount) {
+                return this.login({email,password});
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, login)
+        } catch (error) {
+            console.log("Appwrite :: login :: Error", error);
+        }
+    }
+
+    async getUserAccount() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite :: getUserAccount :: Error", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions()
+       } catch (error) {
+            console.log("Appwrite :: logout :: Error", error);
+        }
+    }
+
+}
+
+const authService = new AuthService;
+
+export default authService;
