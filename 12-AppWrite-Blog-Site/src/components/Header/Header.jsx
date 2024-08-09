d45a7f53d0bf49722ec6d8130fe2926ff722c6d9
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-09 11:43:59 +0530

    Add Logo.jsx and Container.jsx

--- 12-AppWrite-Blog-Site/src/components/Header/Header.jsx
+++ 12-AppWrite-Blog-Site/src/components/Header/Header.jsx
@@ -1,6 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import {Container, Logo, LogoutBtn} from "../index";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
+
+    const authStatus = useSelector((state) => state.auth.status)
+    const navigate = useNavigate()
+
+    const navItems = [
+        {
+            name: "Home",
+            slug: '/',
+            active: true
+        },
+        {
+            name: "Login",
+            slug: '/login',
+            active: !authStatus
+        },
+        {
+            name: "Singup",
+            slug: '/singup',
+            active: !authStatus
+        },
+        {
+            name: "All Posts",
+            slug: '/all-posts',
+            active: authStatus
+        },
+        {
+            name: "Add Post",
+            slug: '/add-post',
+            active: authStatus
+        },
+    ]
     return (
         <div>Header</div>
     )
--- 12-AppWrite-Blog-Site/src/components/index.js
+++ 12-AppWrite-Blog-Site/src/components/index.js
@@ -1,4 +1,13 @@
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
+import LogoutBtn from "./Header/LogoutBtn"
+import Logo from "./Logo"
+import Container from "./container/Container"
 
-export {Header, Footer}
+export {
+    Header,
+    Footer,
+    LogoutBtn,
+    Logo,
+    Container
+}
