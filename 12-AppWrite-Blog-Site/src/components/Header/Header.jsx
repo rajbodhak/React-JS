37e8f60138bdb4674eb81d6c468ed9cb7af86849
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-09 16:33:44 +0530

    Add  navbar in Header.jsx

--- 12-AppWrite-Blog-Site/src/components/Header/Header.jsx
+++ 12-AppWrite-Blog-Site/src/components/Header/Header.jsx
@@ -37,7 +37,36 @@ function Header() {
         },
     ]
     return (
-        <div>Header</div>
+        <header className="py-3 shadow bg-gray-500">
+            <Container>
+                <nav className="flex">
+
+                    <div className="mr-4">
+                        <Link to='/'>
+                            <Logo width="70px" />
+                        </Link> 
+                    </div>
+
+                    <ul className="flex ml-auto">
+                        {navItems.map((item) => item.active ? (
+                            <li key={item.name} className="">
+                                <button
+                                    onClick={() => navigate(item.slug)}
+                                    className="inline-block px-6 py-2 duration-200 rounded-full hover:bg-blue-100">{item.name}
+                                </button>
+                            </li>
+                        ) : null)}
+
+                        {authStatus && (
+                            <li>
+                                <LogoutBtn />
+                            </li>
+                        )}
+                    </ul>
+                    
+                </nav>
+            </Container>
+        </header>
     )
 }
 
