4db504a5191e1acf614d0263d3386424c3196be3
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-11 00:07:12 +0530

    Add PostCard

---
+++ 12-AppWrite-Blog-Site/src/components/PostCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import service from "../Appwrite/mainConfig"
+import {Link} from "react-router-dom"
+
+function PostCard({$id, title, featuredImage}) {
+    return (
+        <Link to={`/post/${$id}`}>
+            <div className="w-full bg-gray-400 rounded-lg p-4">
+                <div className="w-full justify-center mb-4">
+                    <img src={service.getFilePreview(featuredImage)} alt={title} />
+                </div>
+                <h2>{title}</h2>
+            </div>
+        </Link>
+    )
+}
+
+export default PostCard
