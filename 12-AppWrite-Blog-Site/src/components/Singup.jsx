740d3584a4e9cab58053a3c7b8b326a22d019a28
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-11 16:47:51 +0530

    Add  Singup

--- 12-AppWrite-Blog-Site/src/components/Singup.jsx
+++ 12-AppWrite-Blog-Site/src/components/Singup.jsx
@@ -1,27 +1,26 @@
-import React, {useState} from "react"
-import {Link, useNavigate} from "react-router-dom"
-import {useDispatch} from "react-redux"
-import {useForm} from "react-hook-form"
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import {Input, Button, Logo} from "./index"
-import authService from "../Appwrite/auth"
-import { login as authLogin } from "../store/authSlice"
+import { useForm } from "react-hook-form";
+import authService from "../Appwrite/auth";
+import login from "../store/authSlice"
 
-
-function login() {
-
-    const dispatch = useDispatch()
+function Singup() {
     const navigate = useNavigate()
-    const [register, handleSubmit] = useForm()
+    const dispatch = useDispatch()
     const [error, setError] = useState("")
+    const [register, handleSubmit] = useForm()
 
-    const login = async(data) => {
+    const create = async(data) => {
         setError("")
         try {
-            const session = await authService.login(data)
-            if (session) {
-                const userData = await authService.getCurrentUser()
-                if(userData) dispatch(authLogin(userData));
+            const userData = authService.createAccount(data)
+            if(userData) {
+                const currentUserData = authService.getUserAccount()
+                if(currentUserData) dispatch(login(currentUserData));
                 navigate("/")
+
             }
         } catch (error) {
             setError(error.message)
@@ -29,27 +28,34 @@ function login() {
     }
 
     return(
-
-        <div className='flex items-center justify-center w-full'>
+        <div className="flex items-center justify-center">
             <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%"/>
                     </span>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sing up to create an Account</h2>
                 <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp; {/* <- HTML entities */}
-                    <Link 
-                        to="/singup"
+                    Already have an account?&nbsp;
+                    <Link
+                        to="/login"
                         className="font-medium text-primary transition-all duration-200 hover:underline"
                     >
-                        Sing up
+                    Sing in
                     </Link>
                 </p>
-                {error && <p className="text-red-600 mt-8 text-center"> {error} </p>}
-                <form onSubmit={handleSubmit(login)} className='mt-8'>
+                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+                <form onSubmit={handleSubmit(create)}>
                     <div className='space-y-5'>
+                        <Input 
+                            label = "Full Name: "
+                            placeholder = "Enter your full name"
+                            type = "text"
+                            {...register("name", {
+                                required: true
+                            })}
+                        />
                         <Input 
                             label = "Email: "
                             placehokder = "Enter your email"
@@ -71,7 +77,9 @@ function login() {
 
                             })}
                         />
-                        <button type="submit" className="w-full">Sing in</button>
+                        <button type="submit" className="w-full">
+                            Create Account
+                        </button>
                     </div>
                 </form>
             </div>
@@ -79,4 +87,4 @@ function login() {
     )
 }
 
-export default login
+export default Singup
