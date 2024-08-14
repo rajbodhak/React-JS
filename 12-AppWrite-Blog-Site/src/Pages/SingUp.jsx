ac96f20e858e244e038734fca3bbc098a68b30e0
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-14 16:08:53 +0530

    Add Login and Singup Page

---
+++ 12-AppWrite-Blog-Site/src/Pages/SingUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Singup as SingUpComponents} from '../components'
+
+
+function SingUp() {
+  return (
+    <div className='w-8'>
+        <SingUpComponents />
+    </div>
+  )
+}
+
+export default SingUp
