0a4b0d79996a4b799814788a1cc81b2dce8f1650
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-15 00:08:12 +0530

    Add Home Page

--- 12-AppWrite-Blog-Site/src/Pages/AllPost.jsx
+++ 12-AppWrite-Blog-Site/src/Pages/AllPost.jsx
@@ -1,25 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, PostCard } from '../components'
 import service from '../Appwrite/mainConfig'
 
 function AllPost() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {}, [])
-    service.getPosts([]).then((posts) => {
-        if(posts) {
-            setPosts(posts.documents)
-        }
-    })
+
+    useEffect(() => {
+        service.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
 
     return (
         <div>
             <Container>
                 <div className='flex flex-wrap'>
-                    {posts.map((post) => {
-                      <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard post = {post}/>
-                      </div>  
-                    })}
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard post={post} />
+                        </div>
+                    ))}
                 </div>
             </Container>
         </div>
