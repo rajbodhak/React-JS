d646352203224fecb0d4d5bf181c508f4746c866
Raj Bodhak <rajlaptop2004@gmail.com>
2024-08-16 01:07:20 +0530

    Add AllPosts

---
+++ 12-AppWrite-Blog-Site/src/Pages/AllPosts.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+import { Container, PostCard } from '../components'
+import service from '../Appwrite/mainConfig'
+
+function AllPosts() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        service.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard post={post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default AllPosts
