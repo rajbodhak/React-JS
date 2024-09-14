9561d2307412a3bd66a52152bf9366ea810ca6ff
Raj Bodhak <rajlaptop2004@gmail.com>
2024-09-14 14:54:40 +0530

    Context API

---
+++ 13.5-React-Revision/02-Prop-drilling/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useContext } from 'react'
+import { CountContext } from './components/CountContext'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <CountContext.Provider value={count}>
+      <Count count = {count} setCount={setCount}/> 
+      </CountContext.Provider>
+    </div>
+  )
+}
+
+function Count({ setCount}) {
+  return <div>
+    < CountRenderer />
+    <Button setCount = {setCount} />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext)
+  return <div>
+    {count}
+  </div>
+}
+
+function Button({ setCount}) {
+  const count = useContext(CountContext)
+ return <div>
+  <button onClick={() => {setCount(count+1)}}>
+    Increase
+  </button>
+  <button onClick={() => {setCount(count-1)}}>
+    Decrease
+  </button>
+ </div>
+}
+
+export default App
